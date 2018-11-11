f2f3ae28415008ecbcbed1477aaa54f89fd7f682
dima <dima@localhost.localdomain>
2018-11-11 23:29:22 +0200

    add readme

--- kiali/init.sh
+++ kiali/init.sh
@@ -8,8 +8,12 @@ curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/opens
   ISTIO_NAMESPACE=istio-system  \
   GRAFANA_URL=${GRAFANA_URL} envsubst | oc create -n istio-system -f -
 
+sleep 5
+
 curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali-secrets.yaml | \
   VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n istio-system -f -
+
+sleep 5
 
 curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali.yaml | \
   VERSION_LABEL=${VERSION_LABEL}  \
@@ -18,3 +22,5 @@ curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/opens
   NAMESPACE=istio-system  \
   VERBOSE_MODE=4  \
   IMAGE_PULL_POLICY_TOKEN="imagePullPolicy: Always" envsubst | oc create -n istio-system -f -
+
+sleep
